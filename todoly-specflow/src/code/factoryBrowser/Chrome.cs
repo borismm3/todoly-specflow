5b4065cf0869b7ea88d359a9a4f0b4fe244c7c30
Boris Maidana <borismaidana3@gmail.com>
2023-06-02 11:32:22 -0400

    Chrome driver plugin installed

--- todoly-specflow/src/code/factoryBrowser/Chrome.cs
+++ todoly-specflow/src/code/factoryBrowser/Chrome.cs
@@ -1,5 +1,8 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
 
 namespace todoly_specflow.src.code.factoryBrowser
 {
@@ -7,15 +10,16 @@ namespace todoly_specflow.src.code.factoryBrowser
     {
         public IWebDriver Create()
         {
-            string path = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
             ChromeOptions options = new ChromeOptions();
-            /*options.AddArgument("--headless"); // Run Chrome in headless mode
-            options.AddArgument("--no-sandbox"); // Add necessary arguments for headless mode to work properly
+            options.AddArgument("--headless");
+            options.AddArgument("--no-sandbox");
             options.AddArgument("--disable-gpu");
-            options.AddArgument("--disable-dev-shm-usage");*/
+            options.AddArgument("--disable-dev-shm-usage");
 
-            IWebDriver driver = new ChromeDriver(path + "/resources/driver/chromedriver.exe");
+            new DriverManager().SetUpDriver(new ChromeConfig());
+            IWebDriver driver = new ChromeDriver(options);
             driver.Manage().Window.Maximize();
+
             return driver;
         }
     }
