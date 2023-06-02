2670c6f2f390b51f3fbb8302cc81ca72f4bf5cf9
Boris Maidana <borismaidana3@gmail.com>
2023-06-02 13:00:26 -0400

    Path updated

--- todoly-specflow/src/code/factoryBrowser/Chrome.cs
+++ todoly-specflow/src/code/factoryBrowser/Chrome.cs
@@ -19,6 +19,26 @@ namespace todoly_specflow.src.code.factoryBrowser
             string path = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
             Console.WriteLine("FIRST PAR OF PATH = " + path);
             Console.WriteLine("SECOND PAR OF PATH = /resources/driver/chromedriver.exe");
+
+            string directoryPath = path;
+
+            // Get all files in the directory
+            string[] files = Directory.GetFiles(directoryPath);
+            Console.WriteLine("Files:");
+            foreach (string file in files)
+            {
+                Console.WriteLine(file);
+            }
+
+            // Get all subdirectories in the directory
+            string[] directories = Directory.GetDirectories(directoryPath);
+            Console.WriteLine("Directories:");
+            foreach (string directory in directories)
+            {
+                Console.WriteLine(directory);
+            }
+
+
             IWebDriver driver = new ChromeDriver(path + "/resources/driver/", options);
             
             driver.Manage().Window.Maximize();
